b54668cb61a98a1d3252f7a634cfe54391d94a13
name <>
2023-02-06 14:36:22 +0200

    commit4

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,8 +7,14 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int[] b = { 2, 5, 2, 6, 1, 7, 8, 1, 9, 6, 0, 7, 34, 61, 67, 12, 97, 34 };
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(IntToBinary(a));
+            Console.WriteLine(IsEven(a));
+            Console.WriteLine(Max(b));
+            Console.WriteLine(Min(b));
+            Console.WriteLine(Sum(b));
+            Console.WriteLine(Average(b));
         }
         public static string IntToBinary(int n)
         {
@@ -16,12 +22,58 @@ namespace ConsoleApp3
             while(0<n)
             {
                 n2 += n % 2;
-                n/= 2;
+                n /= 2;
             }
             char[] charArray = n2.ToCharArray();
             Array.Reverse(charArray);
             n2 = new string(charArray);
             return n2;
+        }
+        public static bool IsEven(int n)
+        {
+            return (n % 2 == 0);
+        }
+        public static int Max (int[] n)
+        {
+            int n2=0;
+            foreach (int i in n)
+            {
+                if (i > n2)
+                {
+                    n2 = i;
+                }
+            }
+            return n2;
+        }
+        public static int Min(int[] n)
+        {
+            int n2 = 0;
+            foreach (int i in n)
+            {
+                if (i < n2)
+                {
+                    n2 = i;
+                }
+            }
+            return n2;
+        }
+        public static int Sum(int[] n)
+        {
+            int n2=0;
+            foreach (int i in n)
+            {
+                n2 += i;
+            }
+            return n2;
+        }
+        public static int Average(int[] n)
+        {
+            int n2 = 0;
+            foreach (int i in n)
+            {
+                n2 += i;
+            }
+            return n2 /= n.Length;
         }
     }
 }
